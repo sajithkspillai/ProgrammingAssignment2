27cd6cf7bd4dd60195268aa0bfe53e2afca641fe
sajith <sajithkspillai@hotmail.com>
2020-05-01 04:20:03 +0530

    Codes added as requested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makeCacheMatrix take matrix as input and output a special matrix object,list of functions 
+## with matrix value.The second function takes the special matrix object as input and share the cached 
+## inversion matrix of the matrix held by the object. if the cached matrix is missing then it is recalculated. 
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix function has two functions setCache and getCache which set inverse and get the original Matrix
+## input used in the function. Additional check placed to confirm input is a Matrix and square.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-x
+  if(class(x)!="matrix"){stop("not a matrix")}
+  if(nrow(x)!=ncol(x)){stop("not square matrix")}
+  
+  setCache <-function(x= m){
+    y<<-solve(x)
+  }
+  getCache<-function() x
+  list(set=setCache,get=getCache)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function take Matrix object as input with inversed matrix as input and provide the original matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  m<-x$get()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
 }
